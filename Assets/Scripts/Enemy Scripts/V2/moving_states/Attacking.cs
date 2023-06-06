efde38794c312f6d76ca4c7561559c8acfaf35bd
Aleks334 <aleksander.nowicki04@gmail.com>
2023-06-06 16:28:19 +0200

    Resolve merge conflicts - attacking class

--- Assets/Scripts/Enemy Scripts/V2/moving_states/Attacking.cs
+++ Assets/Scripts/Enemy Scripts/V2/moving_states/Attacking.cs
@@ -1,24 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Attacking : moving_state
 {
-    [SerializeField] private AudioCueSO _attackAudioCue;
-    private AudioCue _audioCue;
-
+    // Start is called before the first frame update
     public void Start()
     {
+        name = "Attacking";
         speed = 0;
         nav = this.gameObject.GetComponent<NavMeshAgent>();
-
-        _audioCue = AudioCueComponent;
-    }
-
-    public override void on_state_enter()
-    {
-        base.on_state_enter();
-
-        _audioCue.AudioData = _attackAudioCue;
-        PlaySound();
     }
 }
